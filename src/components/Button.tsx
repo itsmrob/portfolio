28913b34f0427a7f36cc98ab23556bfff0f9d601
Roberto <Robert_jhs@hotmail.com>
2024-03-04 09:52:01 -0600

    button added

---
+++ src/components/Button.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+interface ButtonProps {
+    type: 'button' | 'submit';
+    title: string;
+    icon?: string;
+    variant: string;
+    full?: boolean;
+    onClick: () => void;
+}
+
+const Button = ({ type, title, icon, variant, full, onClick }: ButtonProps) => {
+    return (
+        <button
+            className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`}
+            type={type}
+            onClick={onClick}
+        >
+            {icon && <Image src={icon} alt={title} width={24} height={24} />}
+            <label className="bold-16 whitespace-nowrap cursor-pointer">{title}</label>
+        </button>
+    )
+}
+
+export default Button
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,19 +1,29 @@
+'use client'
 import React from 'react';
+import Button from './Button';
 
 const HeroSection: React.FC = () => {
     return (
         <section className='container lg:mx-auto py-12 pb-20'>
             <div className="flex flex-col gap-4">
                 <div className="w-3/4">
-                    <h2 className="text-lg font-semibold robotoslab-font" style={{ color: "#ff335f" }}>HELLO, MY NAME IS ROB </h2>
+                    <h2 className="text-lg font-semibold robotoslab-font" style={{ color: "#ff335f" }}>HELLO, MY NAME IS ROB</h2>
                 </div>
                 <div className="flex flex-col">
                     <div className='py-5'>
-                        <h1 className="text-4xl sm:text-7xl opacity-60" >I build web and mobile apps.</h1>
+                        <h1 className="text-4xl sm:text-7xl opacity-60" >I build amazing web and mobile apps.</h1>
                     </div>
                     <p className="text-xl sm:text-3xl mt-4 robotoslab-font">
-                        I'm a software engineer and full stack developer. I can make your ideas real.<br />
+                        I'm a software engineer and full stack developer, I can make your ideas real.<br />
                     </p>
+                    <div className='pt-5 pb-5 flex justify-center lg:justify-start'>
+                        <Button
+                            title='Explore my work'
+                            type='button'
+                            variant='btn btn-warning text-black robotoslab-font'
+                            onClick={() => console.log('Click the button')}
+                        />
+                    </div>
                 </div>
             </div>
         </section>
